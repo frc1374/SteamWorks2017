4de817a5cce601d9ab055ed7d533e7e70f2697d8
Gabriel Ciolac <gabrielciolac@gmail.com>
2017-01-08 13:10:28 -0500

    Added GradleRioTemplate

---
+++ src/main/java/org/usfirst/frc/team1374/robot/OI.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team1374.robot;
+
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+    /**Usage:
+     *
+     * private static final Xbox360Controller JS_DRIVER = new Xbox360Controller(int port,double deadzone)
+     *
+     * Example:
+     *
+     * public static void getDriverSpeed()
+     * {
+     * return DRIVER.getRightTrigger() - DRIVER.getLeftTrigger()
+     * }
+     */
+}
+
