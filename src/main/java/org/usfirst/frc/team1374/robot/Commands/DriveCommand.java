e2d6e9eae153d6aa83b9d13d4238353f17ed83e0
Gabriel Ciolac <gabrielciolac@gmail.com>
2017-01-09 00:35:00 -0500

    Updated .gitignore, added some subsystems
    Subsystems may come into use in future commits

---
+++ src/main/java/org/usfirst/frc/team1374/robot/Commands/DriveCommand.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team1374.robot.Commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Created by Gabriel on 2017-01-08.
+ */
+public class DriveCommand extends Command {
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+}
---
+++ src/main/java/org/usfirst/frc/team1374/robot/Subsystems/DriveSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1374.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by Gabriel on 2017-01-08.
+ */
+public class DriveSubsystem extends Subsystem {
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
