3eba105d225b9366f4334be7315d53b0cadcbc7e
mayfarhad <1farhadmay@hdsb.ca>
2017-03-22 19:29:33 -0400

    initialized the gear subsystem
    three solenoids

--- src/main/java/org/usfirst/frc/team1374/robot/Subsystems/GearDellisioSubsystem.java
+++ src/main/java/org/usfirst/frc/team1374/robot/Subsystems/GearDellisioSubsystem.java
@@ -1,14 +1,25 @@
 package org.usfirst.frc.team1374.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.Solenoid;
+
 /**
  * Created by Gabriel on 2017-01-12.
  */
 public class GearDellisioSubsystem {
 
-    /*
-    Three Solenoids
-    1 to allign the gear
-    1 to clamp the gear
-    1 to lift the gear
-     */
+    Solenoid one = new Solenoid(0);
+    Solenoid two = new Solenoid(1);
+    Solenoid three = new Solenoid(2);
+
+    public void une(boolean uno){
+        one.set(true);
+    }
+
+    public void deux(boolean dos){
+        two.set(true);
+    }
+
+    public void trois(boolean tres){
+        three.set(true);
+    }
 }
