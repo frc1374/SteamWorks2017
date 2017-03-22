df85b6054a85fa86d89924c33fac9d1623b0c267
mayfarhad <1farhadmay@hdsb.ca>
2017-03-22 19:36:20 -0400

    solenoids in robot map

--- src/main/java/org/usfirst/frc/team1374/robot/Subsystems/GearDellisioSubsystem.java
+++ src/main/java/org/usfirst/frc/team1374/robot/Subsystems/GearDellisioSubsystem.java
@@ -1,15 +1,16 @@
 package org.usfirst.frc.team1374.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
+import org.usfirst.frc.team1374.robot.RobotMap;
 
 /**
  * Created by Gabriel on 2017-01-12.
  */
 public class GearDellisioSubsystem {
 
-    Solenoid one = new Solenoid(0);
-    Solenoid two = new Solenoid(1);
-    Solenoid three = new Solenoid(2);
+    Solenoid one = new Solenoid(RobotMap.solenoidOne);
+    Solenoid two = new Solenoid(RobotMap.solenoidTwo);
+    Solenoid three = new Solenoid(RobotMap.solenoidThree);
 
     public void une(boolean uno){
         one.set(true);
